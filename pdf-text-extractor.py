75aceb2095cf4b08b7b91d378036e00694d455ff
Spartan-71 <[anishdabhane71@gmail.com]>
2024-09-28 14:11:32 +0530

    script updated

--- pdf-text-extractor.py
+++ pdf-text-extractor.py
@@ -12,23 +12,26 @@ def extract_text_from_pdf(pdf_path):
 import re
 
 def extract_rules(text):
-    pattern = r'(7\.1\.(?:\d+|1[0-9]|2[0-9]))\s+(.*?)\s*(?:\(Automated\))?\n(Profile Applicability:(.*?)\nDescription:(.*?)\nRationale:(.*?)\nAudit:(.*?)\nRemediation:(.*?)\nDefault Value:(.*?)\nReferences:(.*?)CIS Controls:(.*?)(?=7\.1\.(?:\d+|1[0-9]|2[0-9])|\Z))'
+    cleaned_text = re.sub(r'^\s*•\s+', '', text, flags=re.MULTILINE)
+    final_text = re.sub(r'\s*●\s*●\s*●\s*', '', cleaned_text)
+    pattern = r'(7\.1\.(?:\d+|1[0-9]|2[0-9]))\s+(.*?)\s*(?:\(Automated\))?\n(Profile Applicability:(.*?)Description:(.*?)Rationale:(.*?)\nAudit:(.*?)\nRemediation:(.*?)\nDefault Value:(.*?)\nReferences:(.*?)CIS Controls:(.*?)(?=7\.1\.(?:\d+|1[0-9]|2[0-9])|\Z))'
+    # pattern = r'(7\.1\.(?:\d+|1[0-9]|2[0-9]))\s+(.*?)\s*(?:\(Automated\))?\n(Profile Applicability:(.*?)\n(?:.*?\n)*?Description:\s*(.*?)\nRationale:(.*?)\nAudit:(.*?)\nRemediation:(.*?)\nDefault Value:(.*?)\nReferences:(.*?)CIS Controls:(.*?)(?=7\.1\.(?:\d+|1[0-9]|2[0-9])|\Z))'
 
-    matches = re.finditer(pattern, text, re.DOTALL)
+    matches = re.finditer(pattern, final_text, re.DOTALL)
     
     rules = []
     for match in matches:
         rule = {
             'id': match.group(1).strip(),
             'title':match.group(2).strip(),
-            'profile_applicability': match.group(3).strip(),
-            'description': match.group(4).strip(),
-            'rationale': match.group(5).strip(),
-            'audit': match.group(6).strip(),
-            'remediation': match.group(7).strip(),
-            'default_value': match.group(8).strip(),
-            'references': match.group(9).strip(),
-            'cis_controls': match.group(10).strip()
+            'profile_applicability': match.group(4).strip(),
+            'description': match.group(5).strip(),
+            'rationale': match.group(6).strip(),
+            'audit': match.group(7).strip(),
+            'remediation': match.group(8).strip(),
+            'default_value': match.group(9).strip(),
+            'references': match.group(10).strip(),
+            'cis_controls': match.group(11).strip()
         }
         rules.append(rule)
     
